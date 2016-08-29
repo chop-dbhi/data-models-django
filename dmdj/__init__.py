0d5ccc7ef9d25574cfe7a1ac1e8becd94cfee822
Aaron Browne <aaron0browne@gmail.com>
2016-08-29 09:31:09 -0400

    Increase minor version number
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

--- dmdj/__init__.py
+++ dmdj/__init__.py
@@ -7,8 +7,8 @@ if sha:
 
 __version_info__ = {
     'major': 0,
-    'minor': 3,
-    'micro': 8,
+    'minor': 4,
+    'micro': 0,
     'releaselevel': 'alpha',
     'serial': serial,
     'sha': sha
