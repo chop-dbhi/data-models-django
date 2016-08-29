e363279d344dcf097c4d6fb3d4622f976c2f961a
Aaron Browne <aaron0browne@gmail.com>
2016-08-29 12:08:14 -0400

    Fix CI release version dependency
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

--- ci/version.sh
+++ ci/version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ver=$(python -c "from dmdj import get_version; print(get_version(True))")
 lvl=$(python -c "from dmdj import __version_info__; print(__version_info__['releaselevel'])")
-serial="${BUILD_NUM:-0}"
-sha="${GIT_SHA:-0}"
-sha="${sha:0:8}"
 if [ "${lvl}" != "final" ]; then
+    sha="${GIT_SHA}"
+    test -z "${sha}" && sha=$(git log -1 --pretty=format:"%h")
+    test -z "${sha}" && sha=0
+    sha="${sha:0:8}"
+    serial="${BUILD_NUM:-0}"
     ver="${ver}-${lvl}+${serial}.${sha}"
 fi
-echo "${ver}"
+printf "${ver}"
